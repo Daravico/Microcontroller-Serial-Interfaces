5c055afe275138eeeeea40c10728e9fd2b297cd8
David Vidal <david.vidal97@gmail.com>
2024-03-30 23:10:58 -0700

    Added some comments for documentation

--- src/python/serialConfiguration.py
+++ src/python/serialConfiguration.py
@@ -1,10 +1,22 @@
 from serial import Serial
 import serial.tools.list_ports as SerialTools
 
-def show_ports(self):
+def show_ports():
+    '''
+    This function returns the available ports. Needs to be upgraded to 
+    reduce the information to be returned.
+    '''
     return SerialTools.comports()
 
 def establish_parameters(serial_connection: Serial, port: str, speed: int):
+    '''
+    This function sets the parameters of the serial comunication.
+
+    :serial_connection(Serial): from the PySerial library, instance that
+    establish the serial communication.
+    :port(str): sets the designaded serial port.
+    :speed(int): baudrate at which the communication will be made.
+    '''    
     serial_connection.port = port
     serial_connection.baudrate = speed
 
@@ -22,59 +34,3 @@ def write_message(serial_connection:Serial, message: str):
 def flush_serial(serial_connection:Serial):
     serial_connection.reset_input_buffer()
     serial_connection.readline()
-
-
-
-
-
-'''
-class SerialConnection(serial.Serial):       
-
-    def show_ports(self):
-        
-        List of the avilable ports.
-        return SerialTools.comports()
-    
-        for port, desc, _ in sorted(SerialTools.comports()):
-            print("{}: {}".format(port, desc))
-
-    def establish_parameters(self, port, speed):
-        
-        Receive external parameters to set the port and speed.
-        
-        self.port = port
-        self.speed = speed
-
-    def start_connection(self):
-        
-        Starting the connection, it first make sure that there 
-        is no current connection. Then parameters are established
-        and connection is made. 
-        
-        self.connection.close()
-        self.connection.port = self.port
-        self.connection.baudrate = self.speed
-        self.connection.open()
-
-    def close_connection(self):
-        
-        Used to make an external call of the connection object
-        to be closed. More code can be added if needed.
-        
-        self.connection.close()
-
-
-    def write_message(self, msg):
-        self.connection.write(msg)
-        return True
-
-    def flush_serial(self):
-        
-        In case it is needed, this function flushes the input 
-        buffer and reads a single line in order to clear any 
-        incomplete data.
-        
-        self.connection.reset_input_buffer()
-        self.connection.readline()
-
-'''
